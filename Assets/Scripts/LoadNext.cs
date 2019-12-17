0b04879d9e78c3cc6fe5382af6edebd933f30032
Sonny <sonnylo@hotmail.com>
2019-12-17 14:47:48 +0100

    Cutscenes
    Added a loadscene script for use in loading cutscenes

---
+++ Assets/Scripts/LoadNext.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNext : MonoBehaviour
+{
+
+    public float delay;
+    public string sceneName;
+
+
+    // Start is called before the first frame update
+    // Load next scene after a few seconds
+    void Start()
+    {
+        StartCoroutine(LoadNextAfterDelay(delay));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Load next scene 
+   IEnumerator LoadNextAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
