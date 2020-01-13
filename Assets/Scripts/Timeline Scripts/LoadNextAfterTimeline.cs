4b95413f4a281a8626d701f1cf589f8216944fa0
Sonny <sonnylo@hotmail.com>
2020-01-13 23:09:12 +0100

    Update scene
    Update intro scene

---
+++ Assets/Scripts/Timeline Scripts/LoadNextAfterTimeline.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+
+/*
+ * This script is solely used in a video player to go the nexgt scene. 
+ */
+
+public class LoadNextAfterTimeline : MonoBehaviour
+{
+    [SerializeField] VideoPlayer VideoPlayer; // Drag & Drop the GameObject holding the VideoPlayer component
+    [SerializeField] string SceneName;
+
+    void Start()
+    {
+        // When the video player reaches the end (looppoint) it will then load the next scene
+        VideoPlayer.loopPointReached += LoadScene;
+    }
+
+    void LoadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
