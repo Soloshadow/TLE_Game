30fb4ea81dd528f30a3245716cb5341deb17cbd3
Sonny <sonnylo@hotmail.com>
2020-03-08 00:12:30 +0100

    Added information icon and tips
    Added animated floating icon on the scene to show a popup with tips and hints on fine dusts

---
+++ Assets/Scripts/TriggerInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerInfo : MonoBehaviour
+{
+    public Collider2D coll;
+
+    [SerializeField] Image imageTip;
+
+    private bool isTriggered = false;
+
+    // Use this for initialization
+    /*
+    void Start()
+    {
+        //Check if the isTrigger option on th Collider2D is set to true or false
+        if (coll.isTrigger)
+        {
+            Debug.Log("player enter");
+            Time.timeScale = 0;
+        }
+       
+    }
+    */
+
+    private void Update()
+    {
+        if (isTriggered && Input.GetKeyUp(KeyCode.Return))
+        {
+            imageTip.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isTriggered)
+        {
+            imageTip.gameObject.SetActive(true);
+            Time.timeScale = 0;
+            isTriggered = true;
+        }
+    }
+}
